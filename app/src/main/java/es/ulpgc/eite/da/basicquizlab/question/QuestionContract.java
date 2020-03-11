629f56dd4c7fd8245ff79f0a13cf7d46c7dfb5b5
kilian09 <kilian.garcia106@alu.ulpgc.es>
2020-03-11 17:00:20 +0000

    Añadido el QuestionContract y QuestionPresenter

---
+++ app/src/main/java/es/ulpgc/eite/da/basicquizlab/question/QuestionContract.java
@@ -0,0 +1,16 @@
+package es.ulpgc.eite.da.basicquizlab.question;
+
+public interface QuestionContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface Model {
+
+    }
+}
---
+++ app/src/main/java/es/ulpgc/eite/da/basicquizlab/question/QuestionPresenter.java
@@ -0,0 +1,4 @@
+package es.ulpgc.eite.da.basicquizlab.question;
+
+public class QuestionPresenter implements  QuestionContract.Presenter {
+}
